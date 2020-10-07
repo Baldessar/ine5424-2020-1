7b972f8f4b5cb7d568745fb08b14ce45485a1b1b
Baldessar <gb.baldessar@gmail.com>
2020-10-07 12:51:53 -0300

    Fix kinit and kmain reference issue

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include "c/mmu.h"
 #include "c/mem.h"
 
+extern "C" {
+    int kinit() {
+        return 0;
+    }
+
+    int kmain() {
+        return 0;
+    }
+}
+
+
+
 int main() {
 
     return 0;
